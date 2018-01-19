fec55b222c5cec8eedd3880ee54bab62a7c95438
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-19 16:15:17 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the integer");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("given the number is palindrome");
+}
+else
+{
+printf("given the number is not palindrome");
+return 0;
+}
