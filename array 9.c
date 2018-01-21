255bc98faeade13f53d782e0183d71f1629c4918
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-21 10:19:38 +0530

    Create array 9.c

---
+++ array 9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int add numbers(int n);
+int main()
+{
+int num;
+printf("enter a positive integer:");
+scanf("%d",&num);
+printf("sum=%d",add numbers(num));
+return 0;
+}
+if(n!=0)
+return+add numbers(n-1);
+else
+return n;
+}
