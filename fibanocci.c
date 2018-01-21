d45371980c4a1b06f47c7935511fbd45c6b54679
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-21 10:10:24 +0530

    Create fibanocci.c

---
+++ fibanocci.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void print(fibanocci numbers(int n)
+{
+int f1=0,f2=1,i;
+if(n<1)
+return;
+for(i=1;i<+n;i++)
+{
+printf("%d",f2);
+int next=f1+f2;
+f1=f2;
+f2=next;
+}
+}
+int main()
+{
+printf fibanocci numbers(7);
+return 0;
+}
