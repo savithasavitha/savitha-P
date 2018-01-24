4d3dc8d770a70a688e46be3c4e64d955fba5f6b5
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-24 16:45:34 +0530

    Create index 28.c

---
+++ index 28.c
@@ -0,0 +1,19 @@
+#incliude<stdio.h>
+#define MAXSIZE 1000
+int main()
+{
+int array[MAX SIZE];
+int i,N;
+printf("enter the size of array;");
+scanf("%d",&N);
+printf("enter %d elements in the array:",N);
+for(i=0;i<N;i++)
+{
+scanf("%d",&array[i]);
+}
+printf("\n in array are:");
+for(i=0;i<N;i++)
+printf("%d',array[i]);
+}
+return 0;
+}
