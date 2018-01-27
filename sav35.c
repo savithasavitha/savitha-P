2c508d305235dd299e7b7ed9e80747d14d005534
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-27 16:21:08 +0530

    Create sav35.c

---
+++ sav35.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char stc[20];
+int i=0,count=0;
+gets(stc);
+while(stc[i]!='\0')  
+{
+if(stc[i]>='0' && stc[i]<='9')	
+count++;
+i++;
+}
+
+
+printf("%d",count);
+}
