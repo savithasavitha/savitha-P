8250b6b12d25b60a7675faf90070ea316e029e10
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-27 16:34:30 +0530

    Create sav39.c

---
+++ sav39.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,j,temp;
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<10;i++)
+{
+for(j=i+1;j<10;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("%d",a[9]);
+}
