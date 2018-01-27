380c326364f1bd5de0d3446b08e2a10e2122ca4a
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-27 16:25:48 +0530

    Create ss36.c

---
+++ ss36.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    int d = 0;
+    char str[1000];
+    fgets(str,1000,stdin);
+    
+    for(int i = 0; str[i]!='\0'; i++){
+        if(!isalpha(str[i]) && !isdigit(str[i]))
+        d++;
+        
+    }
+    
+    printf("%d",d);
+    return 0;
+   
+}
