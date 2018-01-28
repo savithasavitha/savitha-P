ee07d890050ecfc81ec61ba0e17e99dcf6df8985
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-28 15:27:16 +0530

    Create sav51.c

---
+++ sav51.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int n,result=0,rem,remin;
+  printf("enter the num");
+  scanf("%d",&n);
+while(n!=0)
+{
+  rem=n%10;
+  result=result*10+rem;
+  n=n/10;
+}
+while(result!=0)
+{
+  remin=result%10;
+  printf("%d\t",remin);
+  result=result/10;
+  
+}
+return 0;
+}
