520a567199f338c177db5622a7bac966c570d9a5
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-28 16:54:28 +0530

    Create sav54.c

---
+++ sav54.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ int n,temp,sum=0;
+ printf("enter the digits");
+ scanf("%d",&n);
+while(n>0)
+{
+ temp=n%10;
+ sum=sum+temp;
+ n=n/10;
+ }
+ printf("%d",sum);
+  }
