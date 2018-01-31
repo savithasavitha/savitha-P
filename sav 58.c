16f92dd515bcfa89ef0a209b74a351eb315b32ed
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-01-31 10:30:58 +0000

    Create sav 58.c

---
+++ sav 58.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the two number");
+	scanf("%d %d",&a,&b);
+	printf("before swap%d %d\n",a,b);
+	
+  a=a^b;
+  b=a^b;
+  a=a^b;
+  printf("after swap%d %d",a,b);
+  return 0;
+}
+
