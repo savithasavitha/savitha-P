f67c03216c371fabb527215e8b89451031eac8f6
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-02-07 16:11:50 +0530

    Create sav61.c

---
+++ sav61.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[30],b[30];
+int i,k;
+printf("enter the string");
+for(i=0;i<k;i++)
+ scanf("%s",&a[i]);
+ scanf("%d",k);
+ for(i=0;i<k;i++)
+ {
+ b[i]=a[i];
+ printf("%s",b[i]);
+ }
+ return 0;
+ }
