d30d4772f1eb1550d5121818fb6da71710dba732
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-02-09 11:14:09 +0000

    Create sav 62.c

---
+++ sav 62.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+    int a[50],i;
+    printf("enter the string :");
+    scanf("%d",&a);
+
+   if(a[i]=='0'||'1')
+   {
+
+   i++;
+
+printf("yes\n");
+   }
+
+    else
+    {
+
+    i++;
+printf("no");
+    }
+    return 0;
+}
