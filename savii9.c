59598f82b3100d9443ace663bb4d889b77698cf5
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-02-19 16:49:11 +0530

    Create savii9.c

---
+++ savii9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int count=1,v;
+	char str[50];
+  printf("Enter the string");
+  gets(str);
+	v=strlen(str);
+	for(int i=0;i<=a;i++)
+	{
+		if(str[i]==' ')
+		{
+		    count++;
+		}
+	}
+	printf("The no of space in the line is %d\n",count);
+	return o;
+}
