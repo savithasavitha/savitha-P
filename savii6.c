9782d87346b097ffdb593cc71b1feba6820265ea
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-02-19 16:24:49 +0530

    Create savii6.c

---
+++ savii6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,j;
+char a[10],b[10];
+scanf(%s,&a);
+scanf(%s,&b);
+j=strlen(a);
+for(i=0;i<=j;i++)
+{if (a[i]==a[i+1]&&b[i]==b[i+1])
+printf("yes");
+}
+return 0;
+}
