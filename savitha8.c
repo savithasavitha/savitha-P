2e7c3292f5e22f717b5c6b8567ca7b62d4c039d6
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-02-19 16:44:14 +0530

    Create savitha8.c

---
+++ savitha8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int a,b;
+printf("enter the value",a);
+scanf("%s",&a);
+printf("enter the value",b);
+scanf("%s",&b);
+}
+else
+{
+printf("%s is changed the value");
+return 0;
+}
