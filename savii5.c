f669da80ffa727c02ec7c74a7f6105ce00a85c49
savithasavitha <31506111+savithasavitha@users.noreply.github.com>
2018-02-19 16:21:38 +0530

    Create savii5.c

---
+++ savii5.c
@@ -0,0 +1,21 @@
+#include<Stdio.h>
+int main()
+{
+int a;
+int c;
+printf("enter the values");
+c=a<20
+if(c=0)
+{
+printf("the given number is even");
+}
+else
+{
+printf("the given number is odd");
+}
+return 0;
+}
+
+
+
+
